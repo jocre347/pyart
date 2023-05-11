46cc0acc6563e4c58024172ab51d2a59eda8e539
jocre347 <cremonese.joris@gmail.com>
2023-05-11 13:08:06 +0200

    new

--- tutorial/part1.py
+++ tutorial/part1.py
@@ -19,5 +19,15 @@ arc = w.create_arc(coord, start=0, extent=90, fill="black")
 # so for an arc you need the same type of points as for an rectangle but here you can say how mucht of an circle you need
 # and in all creations you can use names for the colors
 
+#the next is the polygon
+#because there aren't funktions to create triangles or other things you need to make them from scratch
+points = 120, 450, 210, 450, 210, 370
+w.create_polygon(points, outline=None, fill="yellow")
+# here we have more than two points because this is a triangle and  ther is another parameter called outline this works
+# also in the other funktions
+
+#the next is the line
+w.create_line(0, 0, 50, 20, fill="red", width=3)
+#there we have again lots of points and a filling but we have also a widht of the line where you can chose how wide your line is
 
 window.mainloop()
